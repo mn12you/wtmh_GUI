6c6c53e5ee86850a92f1db63cfdb644c46440553
kevincho417 <108808064+kevincho417@users.noreply.github.com>
2023-11-17 17:41:35 +0800

    Delete GUI - 複製 directory

--- GUI - 複製/GUI/ecgsignal_generator.py
+++
@@ -1,8 +0,0 @@
-import time
-with open("D:/lab/AHA Text Data/1001.txt", 'r') as f:
-    # 讀取所有行，提取第一個數字
-    for line in f.readlines():
-        num_list = line.split(',')  #拆分為數字列表
-        first_num = int(num_list[0])  #提取第一個元素
-        print(first_num)
-        time.sleep(1/250)
